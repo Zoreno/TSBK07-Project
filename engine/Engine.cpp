6d6b29826eb45a0501690941496816bfdce4ffb3
Zoreno <metallica317@spray.se>
2017-03-06 14:52:22 +0100

    Texture2D kommenterad och lite info dump i engine

--- engine/Engine.cpp
+++ engine/Engine.cpp
@@ -5,6 +5,8 @@ namespace engine
 	void Engine::init()
 	{
 		window = new Window{ 800,600,"MyWindow" };
+
+		dumpInfo(std::cout);
 	}
 
 	void Engine::run()
@@ -18,5 +20,57 @@ namespace engine
 	void Engine::cleanup()
 	{
 		delete window;
+	}
+
+	void Engine::dumpInfo(std::ostream& stream)
+	{
+		bool listExtensions = false;
+
+		int majorVersion;
+		int minorVersion;
+
+		glGetIntegerv(GL_MAJOR_VERSION, &majorVersion);
+		glGetIntegerv(GL_MINOR_VERSION, &minorVersion);
+
+		stream << "OpenGL Version: " << majorVersion << "." << minorVersion;
+
+		const char* str;
+
+		if (majorVersion >= 3)
+		{
+			str = reinterpret_cast<const char*>(glGetString(GL_VERSION));
+
+			stream << " (" << str << ")";
+		}
+
+		stream << std::endl;
+
+		str = reinterpret_cast<const char*>(glGetString(GL_VENDOR));
+
+		stream << "Vendor: " << str << std::endl;
+
+		str = reinterpret_cast<const char*>(glGetString(GL_RENDERER));
+
+		stream << "Renderer: " << str << std::endl;
+
+		str = reinterpret_cast<const char*>(glGetString(GL_SHADING_LANGUAGE_VERSION));
+
+		stream << "GLSL Version: " << str << std::endl;
+
+		int numExtensions;
+
+		glGetIntegerv(GL_NUM_EXTENSIONS, &numExtensions);
+
+		stream << "Number of extensions: " << numExtensions << std::endl;
+
+		if (!listExtensions)
+			return;
+
+		for (int i{ 0 }; i < numExtensions; ++i)
+		{
+			str = reinterpret_cast<const char*>(glGetStringi(GL_EXTENSIONS, i));
+
+			stream << "Extension " << i << ": " << str << std::endl;
+		}
 	}
 }
--- engine/Texture2D.h
+++ engine/Texture2D.h
@@ -1,3 +1,13 @@
+/**
+ * @file	Texture2D.h
+ * @Author	Joakim Bertils
+ * @date	2017-03-05
+ * @brief	2-Dimensional texture wrapper class.
+ *
+ * https://www.khronos.org/registry/OpenGL-Refpages/gl4/html/glTexParameter.xhtml
+ * https://www.khronos.org/registry/OpenGL-Refpages/gl4/html/glTexImage2D.xhtml
+ */
+
 #pragma once
 
 #include <GL/glew.h>
@@ -14,39 +24,127 @@
 #include "Color.h"
 #include "EngineDLL.h"
 
-// Texture1D
-// Texture2D
-// Texture3D
-// TextureCubeMap
-// Texture1DArray
-// Texture2DArray
-// Texture3DArray
-// TextureCubeMapArray
+ // Texture1D
+ // Texture2D
+ // Texture3D
+ // TextureCubeMap
+ // Texture1DArray
+ // Texture2DArray
+ // Texture3DArray
+ // TextureCubeMapArray
 
+ /**
+  * @brief Texture Mode.
+  *
+  * Specifies the mode used to read from depth-stencil format textures.
+  * The initial value is DEPTH_COMPONENT.
+  */
 enum class TEXTURE_2D_TEXTURE_MODE
 {
+	/**
+	 * @brief Stencil Index Mode.
+	 *
+	 * If the depth stencil mode is STENCIL_INDEX then the stencil
+	 * component is returned in Rt and the depth component is discarded.
+	 */
 	STENCIL_INDEX,
+
+	/**
+	 * @brief Depth Component Mode.
+	 *
+	 * If the depth stencil mode is DEPTH_COMPONENT, then reads from
+	 * depth-stencil format textures will return the depth component
+	 * of the texel in Rt and the stencil component will be discarded.
+	 */
 	DEPTH_COMPONENT,
+
+	/**
+	 * @brief Invalid value.
+	 */
 	INVALID_VALUE
 };
 
+/**
+ * @brief Texture 2D Compare Function
+ *
+ * Specifies the comparison operator used when TEXTURE_2D_COMPARE_MODE
+ * is set to COMPARE_REF_TO_TEXTURE.
+ */
 enum class TEXTURE_2D_COMPARE_FUNC
 {
+	/**
+	 * @brief Less or equal operator.
+	 */
 	LESS_EQUAL,
+
+	/**
+	 * @brief Greater or equal operator.
+	 */
 	GREATER_EQUAL,
+
+	/**
+	 * @brief Less operator.
+	 */
 	LESS,
+
+	/**
+	 * @brief Greater operator.
+	 */
 	GREATER,
+
+	/**
+	 * @brief Equal operator.
+	 */
 	EQUAL,
+
+	/**
+	 * @brief Not equal operator.
+	 */
 	NOT_EQUAL,
+
+	/**
+	 * @brief Always pass test.
+	 */
 	ALWAYS,
+
+	/**
+	 * @brief Never pass test.
+	 */
 	NEVER,
+
+	/**
+	 * @brief Invalid value.
+	 */
 	INVALID_VALUE
 };
 
+/**
+ * @brief Texture 2D compare mode.
+ *
+ * Specifies the texture comparison mode for currently bound depth textures.
+ */
 enum class TEXTURE_2D_COMPARE_MODE
 {
+	/**
+	 * @brief Compare Ref to Texture.
+	 *
+	 * Specifies that the interpolated and clamped r texture coordinate should
+	 * be compared to the value in the currently bound depth texture. The result
+	 * of the comparison is assigned to the red channel.
+	 */
 	COMPARE_REF_TO_TEXTURE,
+
+	/**
+	 * @brief No comparison.
+	 *
+	 * Specifies that the red channel should be assigned the appropriate value
+	 * from the currently bound depth texture.
+	 */
 	NONE,
+
+	/**
+	 * @brief Invalid value.
+	 */
 	INVALID_VALUE
 };
 
@@ -61,23 +159,116 @@ enum class TEXTURE_2D_SWIZZLE_COMPONENT
 	INVALID_VALUE
 };
 
+/**
+ * @brief Texture Wrap Mode
+ *
+ * Specifies behavouir when coordinates are outside texture bounds.
+ */
 enum class TEXTURE_2D_WRAP
 {
+	/**
+	 * @brief Repeat
+	 *
+	 * Texture will be repeated.
+	 */
 	REPEAT,
+
+	/**
+	 * @brief Mirrored Repeat
+	 *
+	 * Texture will be repeated but mirrored in reference to the adjacent
+	 * entry.
+	 */
 	MIRRORED_REPEAT,
+
+	/**
+	 * @brief Clamp to Edge.
+	 *
+	 * Color will be set to the nearest edge color.
+	 */
 	CLAMP_TO_EDGE,
+
+	/**
+	 * @brief Clamp to border.
+	 *
+	 * Color will be set to the predefined border color set with
+	 * setTextureBorderColor().
+	 */
 	CLAMP_TO_BORDER,
+
+	/**
+	 * @brief Invalid value.
+	 */
 	INVALID_VALUE
 };
 
+/**
+ * @brief Texture 2D Filter Mode.
+ *
+ * Specifies behaviour when minifying or magnifying textures.
+ */
 enum class TEXTURE_2D_FILTERING
 {
+	/**
+	 * @brief Nearest Texel.
+	 *
+	 * Returns the value of the texture element that is nearest (in Manhattan
+	 * distance) to the specified texture coordinates.
+	 */
 	NEAREST,
+
+	/**
+	 * @brief Linear Texel Interpolation.
+	 *
+	 * Returns the weighted average of the four texture elements that are
+	 * closest to the specified texture coordinates.
+	 */
 	LINEAR,
+
+	/**
+	 * @brief Nearest Mipmap Nearest Texel.
+	 *
+	 * Chooses the mipmap that most closely matches the size of the pixel
+	 * being textured and uses the NEAREST criterion (the texture element
+	 * closest to the specified texture coordinates) to produce a texture value.
+	 */
 	NEAREST_MIPMAP_NEAREST,
+
+	/**
+	 * @brief Nearest Mipmap Linear Texel Interpolation.
+	 *
+	 * Chooses the mipmap that most closely matches the size of the pixel being
+	 * textured and uses the LINEAR criterion (a weighted average of the
+	 * four texture elements that are closest to the specified texture
+	 * coordinates) to produce a texture value.
+	 */
 	LINEAR_MIPMAP_NEAREST,
+
+	/**
+	 * @brief Linear Mipmap Nearest Texel.
+	 *
+	 * Chooses the two mipmaps that most closely match the size of the pixel
+	 * being textured and uses the NEAREST criterion (the texture element
+	 * closest to the specified texture coordinates) to produce a texture
+	 * value from each mipmap. The final texture value is a weighted average
+	 * of those two values.
+	 */
 	NEAREST_MIPMAP_LINEAR,
+
+	/**
+	 * @brief Linear Mipmap Linear Texel Interpolation.
+	 *
+	 * Chooses the two mipmaps that most closely match the size of the pixel
+	 * being textured and uses the LINEAR criterion (a weighted average of
+	 * the texture elements that are closest to the specified texture
+	 * coordinates) to produce a texture value from each mipmap. The final
+	 * texture value is a weighted average of those two values.
+	 */
 	LINEAR_MIPMAP_LINEAR,
+
+	/**
+	 * @brief Invalid Value.
+	 */
 	INVALID_VALUE
 };
 
@@ -85,8 +276,21 @@ class ENGINE_API Texture2D
 {
 public:
 
+	/**
+	 * @brief Default constructor.
+	 *
+	 * Creates an empty object.
+	 */
 	Texture2D();
 
+	/**
+	 * @brief Constructor
+	 * @param filePath Path to texture file.
+	 * @param sWrap Wrapping behaviour in S-direction.
+	 * @param tWrap Wrapping behaviour in T-direction.
+	 * @param magFilter Magnifying filter behaviour.
+	 * @param minFilter Minifying filter behaviour.
+	 */
 	explicit Texture2D(
 		const char* filePath,
 		TEXTURE_2D_WRAP sWrap = TEXTURE_2D_WRAP::REPEAT,
@@ -95,11 +299,19 @@ public:
 		TEXTURE_2D_FILTERING minFilter = TEXTURE_2D_FILTERING::LINEAR_MIPMAP_LINEAR
 	);
 
+	/**
+	 * @brief Destructor.
+	 */
 	~Texture2D();
 
 	Texture2D(const Texture2D&) = delete;
+
 	Texture2D& operator=(const Texture2D&) = delete;
 
+	/**
+	 * @brief Move constructor
+	 * @param other Source object
+	 */
 	Texture2D(Texture2D&& other) noexcept;
 
 	/**
@@ -119,31 +331,31 @@ public:
 	 * @return Texture Handle.
 	 */
 	GLuint getHandle() const;
-	
+
 	/**
 	 * @brief Depth-stencil format texture read mode.
-	 * 
+	 *
 	 * Specifies the mode used to read from depth-stencil format textures.
-	 * 
+	 *
 	 * @param mode Desired mode.
 	 */
 	void setDepthStencilTextureMode(TEXTURE_2D_TEXTURE_MODE mode);
 
 	/**
 	 * @brief Texture border color.
-	 * 
+	 *
 	 * Defines the color to be sampled outside the texture if wrap mode is
 	 * set to CLAMP_TO_BORDER.
-	 * 
-	 * @param color Border Color. 
+	 *
+	 * @param color Border Color.
 	 */
 	void setTextureBorderColor(Color color);
 
 	/**
 	 * @brief Texture Base Level
-	 * 
+	 *
 	 * Specifies the index of the lowest defined mipmap level.
-	 * 
+	 *
 	 * @param baseLevel Index.
 	 */
 	void setTextureBaseLevel(GLint baseLevel);
@@ -157,43 +369,145 @@ public:
 	*/
 	void setTextureMaxLevel(GLint maxLevel);
 
+	/**
+	 * @brief Sets the texture compare function.
+	 * @param func Function to set.
+	 */
 	void setTextureCompareFunc(TEXTURE_2D_COMPARE_FUNC func);
 
+	/**
+	 * @brief Sets the texture compare mode.
+	 * @param mode Mode to set.
+	 */
+	void setTextureCompareMode(TEXTURE_2D_COMPARE_MODE mode);
+
+	/**
+	 * @brief Sets the texture Level of Detail bias.
+	 * @param LODBias New value for bias.
+	 */
+	void setTextureLODBias(GLint LODBias);
+
+	/**
+	 * @brief Sets minimum Level of Detail value.
+	 * @param minLOD New value for minimum LOD.
+	 */
+	void setTextureMinLOD(GLint minLOD);
+
+	/**
+	* @brief Sets maximum Level of Detail value.
+	* @param maxLOD New value for maximum LOD.
+	*/
+	void setTextureMaxLOD(GLint maxLOD);
+
+	/**
+	 * @brief Sets the s-coordinate wrap behaviour.
+	 * @param sWrap New behaviour.
+	 */
 	void setTextureWrapS(TEXTURE_2D_WRAP sWrap);
+
+	/**
+	* @brief Sets the t-coordinate wrap behaviour.
+	* @param tWrap New behaviour.
+	*/
 	void setTextureWrapT(TEXTURE_2D_WRAP tWrap);
+
+	/**
+	* @brief Sets the r-coordinate wrap behaviour.
+	* @param rWrap New behaviour.
+	*/
 	void setTextureWrapR(TEXTURE_2D_WRAP rWrap);
 
+	/**
+	 * @brief Set magnifying filter behavour.
+	 * @param magFilter New behaviour.
+	 */
 	void setTextureMagFilter(TEXTURE_2D_FILTERING magFilter);
+
+	/**
+	* @brief Set minifying filter behavour.
+	* @param minFilter New behaviour.
+	*/
 	void setTextureMinFilter(TEXTURE_2D_FILTERING minFilter);
 
+	/**
+	* @brief Gets Depth-stencil format texture read mode.
+	* @return Current Mode.
+	*/
 	TEXTURE_2D_TEXTURE_MODE getDepthStencilTextureMode() const;
 
+	/**
+	 * @brief Gets border color.
+	 * @return Current Color.
+	 */
 	Color getTextureBorderColor() const;
 
+	/**
+	 * @brief Gets mipmap base level.
+	 * @return Current base level.
+	 */
 	GLint getTextureBaseLevel() const;
 
+	/**
+	 * @brief Gets mipmap max level.
+	 * @return Current max level.
+	 */
 	GLint getTextureMaxLevel() const;
 
 	TEXTURE_2D_COMPARE_FUNC getTextureCompareFunc() const;
 
+	TEXTURE_2D_COMPARE_MODE getTextureCompareMode() const;
+
+	GLint getTextureLODBias() const;
+
+	GLint getTextureMinLOD() const;
+
+	GLint getTextureMaxLOD() const;
+
 	TEXTURE_2D_WRAP getTextureWrapS() const;
+
 	TEXTURE_2D_WRAP getTextureWrapT() const;
+
 	TEXTURE_2D_WRAP getTextureWrapR() const;
 
 	TEXTURE_2D_FILTERING getTextureMagFilter() const;
+
 	TEXTURE_2D_FILTERING getTextureMinFilter() const;
 
-	// Bind texture to currently active unit
+	
+	/**
+	 * @brief Bind texture to current unit.
+	 */
 	void bind() const;
 
-	// Bind texture to given unit
+	
+	/**
+	 * @brief Bind texture to specified unit.
+	 * @param texUnit Unit to bind to.
+	 */
 	void bind(GLuint texUnit) const;
+
 private:
+
+	/**
+	 * @brief Width of texture.
+	 */
 	GLuint width;
+	
+	/**
+	 * @brief Height of texture.
+	 */
 	GLuint height;
 
+	/**
+	 * @brief OpenGL texture handle.
+	 */
 	GLuint textureID;
 
+	/**
+	 * @brief Swaps two textures.
+	 * @param first First texture.
+	 * @param second Second texture.
+	 */
 	friend void swap(Texture2D& first, Texture2D& second) noexcept;
 };
 
