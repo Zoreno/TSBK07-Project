d5b59320e359b619d59f18e133d8633de4b34eba
Zoreno <metallica317@spray.se>
2017-03-11 10:15:32 +0100

    UserComponent exempel

--- game/main.cpp
+++ game/main.cpp
@@ -1,12 +1,33 @@
 #include <Engine.h>
 
+#include <ComponentContainer.h>
+#include <Component.h>
+#include <Entity.h>
+#include <iostream>
+
+class UserComponent : public Component
+{
+public:
+	std::string userString;
+};
+
 int main()
 {
 	engine::Engine engine;
 
 	engine.init();
 
+	Entity userEntity;
+
+	UserComponent userComponent;
+
+	userComponent.userString = "Hej jag heter kalle";
+
+	ComponentContainer::addComponent(userEntity.getID(), userComponent);
+
 	engine.run();
+
+	std::cout << ComponentContainer::getComponent<UserComponent>(userEntity.getID())->userString << std::endl;
 
 	engine.cleanup();
 }
